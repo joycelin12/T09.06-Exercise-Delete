11b2c9707c14c5eb476458337b33e20a632afe67
joycelin12 <joycelin12@hotmail.com>
2018-03-15 11:36:30 -0400

    TODO (1) Get access to the database and write URI matching code to recognize a single item TODO (2) Write the code to delete a single row of data // [Hint] Use selections to delete an item by its row ID TODO (3) Notify the resolver of a change and return the number of items deleted

--- app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -157,13 +157,39 @@ public class TaskContentProvider extends ContentProvider {
     public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
 
         // TODO (1) Get access to the database and write URI matching code to recognize a single item
+        final SQLiteDatabase db = mTaskDbHelper.getWritableDatabase();
 
         // TODO (2) Write the code to delete a single row of data
         // [Hint] Use selections to delete an item by its row ID
+        int match = sUriMatcher.match(uri);
+
+        int tasksDeleted;
+
+        switch (match) {
+
+            case TASK_WITH_ID:
+                String id = uri.getPathSegments().get(1);
+                String mSelection ="_id=?";
+                String[] mSelectionArgs = new String[]{id};
+
+                tasksDeleted = db.delete(TABLE_NAME,
+                        mSelection,
+                        mSelectionArgs);
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
 
         // TODO (3) Notify the resolver of a change and return the number of items deleted
+        if (tasksDeleted != 0) {
 
-        throw new UnsupportedOperationException("Not yet implemented");
+                getContext().getContentResolver().notifyChange(uri, null);
+        }
+        // Return the desired Cursor
+        return tasksDeleted;
+
+        //throw new UnsupportedOperationException("Not yet implemented");
     }
 
 
